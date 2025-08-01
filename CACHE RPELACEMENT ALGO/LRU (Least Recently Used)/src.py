9bc247a3f61421a340e18d368b38591c84a62076
HEET AKA ARJUNA <heettrivedi02@gmail.com>
2025-08-01 11:50:57 +0530

    Create src.py

---
+++ CACHE RPELACEMENT ALGO/LRU (Least Recently Used)/src.py
@@ -0,0 +1,32 @@
+class LRUCache:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.cache = {}
+        self.order = []
+
+    def get(self, key):
+        if key not in self.cache:
+            return -1
+        self.order.remove(key)
+        self.order.append(key)
+        print(f"Cache after get({key}): {[(k, self.cache[k]) for k in self.order]}")
+        return self.cache[key]
+
+    def put(self, key, value):
+        if key in self.cache:
+            self.order.remove(key)
+        elif len(self.cache) == self.capacity:
+            oldest = self.order.pop(0)
+            del self.cache[oldest]
+        self.cache[key] = value
+        self.order.append(key)
+        print(f"Cache after put({key}, {value}): {[(k, self.cache[k]) for k in self.order]}")
+
+if __name__ == "__main__":
+    cache = LRUCache(3)
+    cache.put(1, 10)
+    cache.put(2, 20)
+    cache.put(3, 30)
+    cache.put(4, 40)
+    print(f"Get(2) = {cache.get(2)}")
+    cache.put(5, 50)
