5eb1bd964ae9e2e9d59f8f6d8e42647e28790b1a
HEET AKA ARJUNA <heettrivedi02@gmail.com>
2025-08-01 11:48:13 +0530

    Create src.rb

---
+++ CACHE RPELACEMENT ALGO/FIFO (FIRST IN FIRST OUT)/src.rb
@@ -0,0 +1,42 @@
+class FIFOCache
+  def initialize(capacity)
+    @capacity = capacity
+    @cache = {}
+    @order = []
+  end
+
+  def get(key)
+    return -1 unless @cache.key?(key)
+    print "Cache after get(#{key}): ["
+    @order.each_with_index do |k, i|
+      print "(#{k}, #{@cache[k]})" + (i < @order.size - 1 ? ", " : "")
+    end
+    puts "]"
+    @cache[key]
+  end
+
+  def put(key, value)
+    if @cache.key?(key)
+      @cache[key] = value
+    else
+      if @cache.size == @capacity
+        @cache.delete(@order.shift)
+      end
+      @cache[key] = value
+      @order << key
+    end
+    print "Cache after put(#{key}, #{value}): ["
+    @order.each_with_index do |k, i|
+      print "(#{k}, #{@cache[k]})" + (i < @order.size - 1 ? ", " : "")
+    end
+    puts "]"
+  end
+end
+
+cache = FIFOCache.new(3)
+cache.put(1, 10)
+cache.put(2, 20)
+cache.put(3, 30)
+cache.put(4, 40)
+puts "Get(2) = #{cache.get(2)}"
+cache.put(5, 50)
