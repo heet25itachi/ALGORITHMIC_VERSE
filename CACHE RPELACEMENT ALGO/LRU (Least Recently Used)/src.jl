4e9161178bbe8c4d21c098743883ce94ebb5447a
HEET AKA ARJUNA <heettrivedi02@gmail.com>
2025-08-01 11:53:24 +0530

    Create src.jl

---
+++ CACHE RPELACEMENT ALGO/LRU (Least Recently Used)/src.jl
@@ -0,0 +1,43 @@
+mutable struct LRUCache
+    capacity::Int
+    cache::Dict{Int,Int}
+    order::Vector{Int}
+    LRUCache(capacity) = new(capacity, Dict{Int,Int}(), Int[])
+end
+
+function get(cache::LRUCache, key::Int)
+    if !haskey(cache.cache, key)
+        return -1
+    end
+    deleteat!(cache.order, findfirst(isequal(key), cache.order))
+    push!(cache.order, key)
+    print("Cache after get($key): [")
+    for (i, k) in enumerate(cache.order)
+        print("($k, $(cache.cache[k]))" * (i < length(cache.order) ? ", " : ""))
+    end
+    println("]")
+    return cache.cache[key]
+end
+
+function put(cache::LRUCache, key::Int, value::Int)
+    if haskey(cache.cache, key)
+        deleteat!(cache.order, findfirst(isequal(key), cache.order))
+    elseif length(cache.cache) == cache.capacity
+        delete!(cache.cache, popfirst!(cache.order))
+    end
+    cache.cache[key] = value
+    push!(cache.order, key)
+    print("Cache after put($key, $value): [")
+    for (i, k) in enumerate(cache.order)
+        print("($k, $(cache.cache[k]))" * (i < length(cache.order) ? ", " : ""))
+    end
+    println("]")
+end
+
+cache = LRUCache(3)
+put(cache, 1, 10)
+put(cache, 2, 20)
+put(cache, 3, 30)
+put(cache, 4, 40)
+println("Get(2) = $(get(cache, 2))")
+put(cache, 5, 50)
