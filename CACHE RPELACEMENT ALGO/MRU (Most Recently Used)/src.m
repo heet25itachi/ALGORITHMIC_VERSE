23e00a0fb5e785c856872e08544eeeef2e616d28
HEET AKA ARJUNA <heettrivedi02@gmail.com>
2025-08-01 12:04:03 +0530

    Create src.m

---
+++ CACHE RPELACEMENT ALGO/MRU (Most Recently Used)/src.m
@@ -0,0 +1,52 @@
+function mru_cache()
+    capacity = 3;
+    cache = containers.Map('KeyType', 'int32', 'ValueType', 'int32');
+    order = [];
+
+    function value = get(key)
+        if isKey(cache, key)
+            value = cache(key);
+            order(order == key) = [];
+            order = [key, order];
+            fprintf('Cache after get(%d): [', key);
+            for i = 1:length(order)
+                fprintf('(%d, %d)', order(i), cache(order(i)));
+                if i < length(order)
+                    fprintf(', ');
+                end
+            end
+            fprintf(']\n');
+        else
+            value = -1;
+        end
+    end
+
+    function put(key, value)
+        if isKey(cache, key)
+            cache(key) = value;
+            order(order == key) = [];
+        else
+            if length(cache) == capacity
+                remove(cache, order(1));
+                order(1) = [];
+            end
+            cache(key) = value;
+        end
+        order = [key, order];
+        fprintf('Cache after put(%d, %d): [', key, value);
+        for i = 1:length(order)
+            fprintf('(%d, %d)', order(i), cache(order(i)));
+            if i < length(order)
+                fprintf(', ');
+            end
+        end
+        fprintf(']\n');
+    end
+
+    put(1, 10);
+    put(2, 20);
+    put(3, 30);
+    put(4, 40);
+    fprintf('Get(2) = %d\n', get(2));
+    put(5, 50);
+end
