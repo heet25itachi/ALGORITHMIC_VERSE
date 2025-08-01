56b3f62f2d71da40bfc7a2d634e1c509a2afd528
HEET AKA ARJUNA <heettrivedi02@gmail.com>
2025-08-01 11:47:18 +0530

    Create src.scala

---
+++ CACHE RPELACEMENT ALGO/FIFO (FIRST IN FIRST OUT)/src.scala
@@ -0,0 +1,44 @@
+object FIFOCache {
+  class FIFOCache(capacity: Int) {
+    private val cache = scala.collection.mutable.Map[Int, Int]()
+    private val order = scala.collection.mutable.ListBuffer[Int]()
+
+    def get(key: Int): Int = {
+      if (!cache.contains(key)) return -1
+      print(s"Cache after get($key): [")
+      order.zipWithIndex.foreach { case (k, i) =>
+        print(s"($k, ${cache(k)})" + (if (i < order.length - 1) ", " else ""))
+      }
+      println("]")
+      cache(key)
+    }
+
+    def put(key: Int, value: Int): Unit = {
+      if (cache.contains(key)) {
+        cache(key) = value
+      } else {
+        if (cache.size == capacity) {
+          cache.remove(order.head)
+          order.remove(0)
+        }
+        cache(key) = value
+        order += key
+      }
+      print(s"Cache after put($key, $value): [")
+      order.zipWithIndex.foreach { case (k, i) =>
+        print(s"($k, ${cache(k)})" + (if (i < order.length - 1) ", " else ""))
+      }
+      println("]")
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    val cache = new FIFOCache(3)
+    cache.put(1, 10)
+    cache.put(2, 20)
+    cache.put(3, 30)
+    cache.put(4, 40)
+    println(s"Get(2) = ${cache.get(2)}")
+    cache.put(5, 50)
+  }
+}
