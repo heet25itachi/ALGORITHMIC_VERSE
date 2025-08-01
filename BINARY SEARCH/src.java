d267b63d405854b1d7f917bf94067ca83a7da243
HEET AKA ARJUNA <heettrivedi02@gmail.com>
2025-08-01 10:33:58 +0530

    Create src.java

---
+++ BINARY SEARCH/src.java
@@ -0,0 +1,23 @@
+public class BinarySearch {
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0, right = arr.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 4, 7, 9};
+        int target = 9;
+        int result = binarySearch(arr, target);
+        System.out.println("Target " + target + " found at index: " + result);
+    }
+}
